fdc07b61460db86a6fd3247cf02823badf25cbfb
coolwonny <60997174+coolwonny@users.noreply.github.com>
2020-09-21 17:30:10 -0500

    images added

--- README.md
+++ README.md
@@ -1,1 +1,50 @@
-# Wallet
+# Multi-Blockchain Wallet in Python
+
+In this assignment, we are going to create a code in Python that allows us to transact and manage a blockchain wallet not pertaining to one coin but acrosss 300+ coins. For this assignment, we are to make 2 coins working: Ethereum and Bitcoin Testnet. Ethereum keys are the same format on any network, so the Ethereum keys should work with your custom networks or testnets.    
+
+The process can be divided into three parts: 
+- Writing a function that derives the wallet keys
+- Writing a function that links the derived keys to the accouts we want to make a transaction.
+- Testing some transactions using the code.
+
+## Deriving the wallet keys
+
+In this section, we are to create a function using `subprocess` library that derives wallet information such as address, index, path, private key and public key. With this information, we can call each necessary private key for desired transaction.   
+
+## Linking the transaction signing libraries
+
+In this section, we are to create three more functions as below.    
+
+1. `priv_key_to_account` -- this will convert the privkey string in a child key to an account object
+that bit or web3.py can use to transact.
+This function needs the following parameters:
+- coin -- the coin type (defined in constants.py).
+- priv_key -- the privkey string will be passed through here.
+- For ETH, return Account.privateKeyToAccount(priv_key)
+- For BTCTEST, return PrivateKeyTestnet(priv_key)
+
+2. `create_tx` -- this will create the raw, unsigned transaction that contains all metadata needed to transact.
+This function needs the following parameters:
+- coin -- the coin type (defined in constants.py).
+- account -- the account object from priv_key_to_account.
+- to -- the recipient address.
+- amount -- the amount of the coin to send.
+- For ETH, return an object containing `to`, `from`, `value`, `gas`, `gasPrice` and `nonce`
+- For BTCTEST, return `PrivateKeyTestnet.prepare_transaction(account.address, [(to, amount, BTC)])`
+
+3. `send_tx` -- this will call create_tx, sign the transaction, then send it to the designated network.
+This function needs the following parameters:
+- coin -- the coin type (defined in constants.py).
+- account -- the account object from priv_key_to_account.
+- to -- the recipient address.
+- amount -- the amount of the coin to send.
+- For ETH, return `w3.eth.sendRawTransaction(signed.rawTransaction)`
+- For BTCTEST, return `NetworkAPI.broadcast_tx_testnet(signed)`    
+
+Once you've signed the transaction, you will need to send it to the designated blockchain network.    
+
+
+
+
+
+
--- wallet.py
+++ wallet.py
@@ -2,14 +2,21 @@ from constants import *
 import subprocess
 import json
 import os
+from dotenv import load_dotenv
+from web3 import Web3
+from web3.middleware import geth_poa_middleware
+from eth_account import Account
+from pathlib import Path
+from getpass import getpass
+from bit import PrivateKeyTestnet
+from bit.network import NetworkAPI
 
-# mnemonic = os.getenv('MNEMONIC', 'van spirit reward accident opera load option slow fortune acid picnic genuine')
-# The php command that will get our derived addresses
-# --coin ETH will derive ethereum addresses
-# --format=json changes the terminal output to json format
+# Load mnemonic in env file and assign it a variable called 'mnemonic'
+load_dotenv('c:/Users/Sungwon Kim/fintech/.env')
+mnemonic = os.getenv('MNEMONIC_2')
+
 def derive_wallets(mnemonic, coin, numderive):
-    command = f'php hd-wallet-derive/hd-wallet-derive.php -g --mnemonic="{mnemonic}" --coin ="{coin}" --numderive="{numderive}" --cols=path,address,privkey,pubkey --format=json'
-
+    command = f'php hd-wallet-derive/hd-wallet-derive.php -g --mnemonic="{mnemonic}" --coin="{coin}" --numderive="{numderive}" --cols=address,index,path,privkey,pubkey,pubkeyhash,xprv,xpub --format=json'
     p = subprocess.Popen(command, stdout=subprocess.PIPE, shell=True)
     (output, err) = p.communicate()
     p_status = p.wait()
@@ -17,4 +24,51 @@ def derive_wallets(mnemonic, coin, numderive):
     keys = json.loads(output)
     # print all the addresses
     return keys
+
+# Create an object called coins that derives ETH and BTCTEST wallets with this function
+coin = [ETH, BTCTEST]
+# Create a dictionary comprehension for the list of coin.
+coins = {k:v for k in coin for v in [derive_wallets(mnemonic, k, 3)]}
+
+#  Add middleware to web3.py to support the PoA algorithm
+w3 = Web3(Web3.HTTPProvider("http://127.0.0.1:8545"))
+w3.middleware_onion.inject(geth_poa_middleware, layer=0)
+
+def priv_key_to_account(coin, priv_key):
+    if coin == ETH:
+        return Account.privateKeyToAccount(priv_key)
+    elif coin == BTCTEST:
+        return PrivateKeyTestnet(priv_key)
+
+# For convenience, contain the account information by assigning them variables
+eth_account = priv_key_to_account(ETH, coins['eth'][0]['privkey'])
+btc_account = priv_key_to_account(BTCTEST, coins['btc-test'][0]['privkey'])
+
+def create_tx(coin, account, to, amount):
+    if coin == ETH:
+        gasEstimate = w3.eth.estimateGas(
+        {"from": account.address, "to": to, "value": amount}
+    )
+        return {
+            "to": to,
+            "from": account.address,
+            "value": amount,
+            "gas": gasEstimate,
+            "gasPrice": w3.eth.gasPrice,
+            "nonce": w3.eth.getTransactionCount(account.address),
+            
+    }
+    elif coin == BTCTEST:
+        return PrivateKeyTestnet.prepare_transaction(account.address, [(to, amount, BTC)])
+
+def send_tx(coin, account, to, amount):
+    tx = create_tx(coin, account, to, amount)
+    if coin == ETH:
+        signed_tx = account.sign_transaction(tx)
+        result = w3.eth.sendRawTransaction(signed_tx.rawTransaction)
+        return result.hex()
+    elif coin == BTCTEST:
+        signed_tx = account.sign_transaction(tx)
+        return NetworkAPI.broadcast_tx_testnet(signed_tx)
+
 
